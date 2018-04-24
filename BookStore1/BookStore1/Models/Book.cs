d5097ed866557407520e14187029ac29610509ff
ayeMoney24 <32275218+ayeMoney24@users.noreply.github.com>
2018-04-24 17:28:49 -0400

    Added models

---
+++ BookStore1/BookStore1/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Genere { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
---
+++ BookStore1/BookStore1/Models/Merch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore1.Models
+{
+    public class Merch
+    {
+        public int id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double price { get; set; }
+
+    }
+}
