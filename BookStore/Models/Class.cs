99eb84c3d3c8efe8b0d426843986395f066e8af4
ayeMoney24 <32275218+ayeMoney24@users.noreply.github.com>
2018-05-01 12:50:08 -0400

    updated sln file

---
+++ BookStore/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public string Description { get; set; }
+		public string genere { get; set; }
+		public decimal Price { get; set; }
+		public int BookId { get; set; }
+		
+	}
+}
---
+++ BookStore/Services/BookServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Services
+{
+    public class BookServices
+    {
+
+
+    }
+}
